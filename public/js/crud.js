786497266a5ce43046571da57c891cc00ad57eb9
Leandro Roberto Home <leroberto@gmail.com>
2018-04-16 23:41:29 -0300

    JS CNPJ/Tax Number

--- public/js/crud.js
+++ public/js/crud.js
@@ -1,6 +1,35 @@
 $(document).ready(function(){
+        
+    //$('#emp_cnpj').mask('00.000.000/0000-00', {reverse: true});
 
-    $('#emp_cnpj').mask('00.000.000/0000-00', {reverse: true});
+    var cnpj_value = $("input[name='empresa']:checked").val();
+    setCnpj(cnpj_value);
+
+    function setCnpj(value)
+    {
+        if (value == '1') {
+            $('#emp_cnpj').mask('00.000.000/0000-00', {reverse: true});
+        }
+        elseif(value == '2')
+        {
+            $('#emp_cnpj').mask('00000000000000', {reverse: true});
+        }
+    }
+
+    /*$('input[type=radio][name=empresa]').change(function() {
+        if (this.value == '1') {
+            $('#emp_cnpj').mask('00.000.000/0000-00', {reverse: true});
+        }
+        else if (this.value == '2') {
+            $('#emp_cnpj').mask('00000000000000', {reverse: true});
+        } 
+    });*/
+    
+    /*$('#empresa').change(function(){
+        selected_value = $("input[name='empresa']:checked").val();
+        alert(selected_value);
+    });*/
+
 
 });
   
